7d13d175b6edf64a2af6564748935778e1a9c9d7
Mohammad A <mohammed.abuelhagag@gmail.com>
2019-08-14 02:23:21 +0200

    init commit
    generated by stagehand

---
+++ example/youtube_thumbnails_example.dart
@@ -0,0 +1,6 @@
+import 'package:youtube_thumbnails/youtube_thumbnails.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/youtube_thumbnails.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library youtube_thumbnails;
+
+export 'src/youtube_thumbnails_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
